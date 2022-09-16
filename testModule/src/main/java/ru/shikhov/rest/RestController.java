c0df717a2984bfaedb05ffdfdcde0df3e5b986d9
sAyflort <neefe.saratan.life.life@gmail.com>
2022-09-16 16:53:47 +0300

    prepare hm12

--- testModule/src/main/java/ru/shikhov/rest/RestController.java
+++ testModule/src/main/java/ru/shikhov/rest/RestController.java
@@ -3,61 +3,11 @@ package ru.shikhov.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.shikhov.exceptions.EntityNotFoundException;
 import ru.shikhov.model.dto.ProductDto;
 import ru.shikhov.service.ProductService;
 
-import javax.validation.Valid;
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
-
-/*@RequestMapping("/api/v1/students")
-@RestController
-public class StudentsRestController {
-private StudentsService studentsService;
-@Autowired
-public void setStudentsService(StudentsService studentsService) {
-this.studentsService = studentsService;
-}
-@GetMapping("/{id}")
-public Student getStudentById(@PathVariable Long id) {
-return studentsService.getStudentById(id);
-}
-@GetMapping
-public List<Student> getAllStudents() {
-return studentsService.getAllStudentsList();
-}
-© geekbrains.ru 6
-@PostMapping
-public Student addStudent(@RequestBody Student student) {
-student.setId(0L);
-return studentsService.saveOrUpdate(student);
-}
-@PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-public Student updateStudent(@RequestBody Student student) {
-return studentsService.saveOrUpdate(student);
-}
-@DeleteMapping("/{id}")
-public int deleteStudent(@PathVariable Long id) {
-studentsService.delete(id);
-return HttpStatus.OK.value();
-}
-@ExceptionHandler
-public ResponseEntity<StudentsErrorResponse>
-handleException(StudentNotFoundException exc) {
-StudentsErrorResponse studentsErrorResponse = new
-StudentsErrorResponse();
-studentsErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-studentsErrorResponse.setMessage(exc.getMessage());
-studentsErrorResponse.setTimestamp(System.currentTimeMillis());
-return new ResponseEntity<>(studentsErrorResponse,
-HttpStatus.NOT_FOUND);
-}
-}
-*/
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/products")
@@ -79,7 +29,7 @@ public class RestController {
     }
 
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}/id")
     public ProductDto form(@PathVariable("id") long id) {
         return service.findProductById(id).orElse(new ProductDto());
     }
@@ -92,7 +42,15 @@ public class RestController {
     }
 
     @PutMapping
-    public ProductDto saveProduct(@RequestBody ProductDto product) {
+    public ProductDto updateProduct(@RequestBody ProductDto product) {
+        service.save(product);
+        return product;
+    }
+    @PostMapping
+    public ProductDto saveProduct(@RequestBody ProductDto product) throws IllegalAccessException {
+        if(product.getId() != null) {
+            throw new IllegalAccessException("Created product shouldn`t have id");
+        }
         service.save(product);
         return product;
     }
